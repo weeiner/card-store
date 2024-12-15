13ce4100943992098747f14abd87c332d6426b0d
weeiner <71634275+weeiner@users.noreply.github.com>
2024-12-15 17:35:07 +0800

    feat: added shadcn

--- poke-weenie/src/pages/Home.jsx
+++ poke-weenie/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-amber-950">testing!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
